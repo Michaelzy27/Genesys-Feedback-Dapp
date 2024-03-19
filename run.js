97315826febee1e679e72bc66ab9e36210455f61
Michaelzy27 <umehmichael27@gmail.com>
2024-03-19 23:51:33 +0100

    update

--- run.js
+++ run.js
@@ -4,7 +4,7 @@ addFeedbackButton.addEventListener("click", () => {addFeedback();});  //when sub
 function addFeedback() {
     const newFeedback = document.getElementById("thefeedback").value;      //gets the feedback from the text field
     //helloContract.addFeedback(newFeedback);
-    document.getElementById("thefeedback").value = "hhh";
+    document.getElementById("thefeedback").value = "";
 }
 
 const connectWalletButton = document.getElementById("connect");
@@ -15,17 +15,15 @@ const connectWalletButton = document.getElementById("connect");
 
 function connectWallet() {
     if (typeof window.ethereum !== 'undefined') {
-        const web3 = new Web3(window.ethereum); // Moved this line inside the if statement
+        const web3 = new Web3(window.ethereum);
         try {
           await window.ethereum.request({ method: "eth_requestAccounts" });
           const accounts = await web3.eth.getAccounts();
           const walletAddress = accounts[0];
-          //const walletAddressElement = document.getElementById('walletAddress');
-          //walletAddressElement.textContent = walletAddress;
         } catch (error) {
           console.error(error);
         }
       } else {
-        console.error('No web3 provider detected. Please install MetaMask.');
+        console.error('No web3 provider detected. Please install MetaMask.');   //unabe to complete due to country network outage
       }
 }
